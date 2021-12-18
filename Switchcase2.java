bf418e2a93167d8083b4862de241f9e3c596aad0
Yogendra Swami <yogendra.epc@gmail.com>
2021-12-18 19:21:10 +0530

    uhgfd

---
+++ Switchcase2.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.util.Scanner;
+class Switchcase {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+	
+	
+	for(int i=0; i<4; i++) {
+		System.out.println(2*i);
+		
+	}
+	for(int i=0;i<=10;i=i+2) {
+		
+	}
+}
+}
+
+
+
+
+
+
+
